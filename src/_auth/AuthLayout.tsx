5cc26d0dc2d897d5aad39b8ad4ba2b35236aab0e
Josiah Voight <jovoight@gmail.com>
2024-02-27 03:47:49 -0600

    added sample project assets folder

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,10 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center">
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+          <img />
         </>
       )}
     </>

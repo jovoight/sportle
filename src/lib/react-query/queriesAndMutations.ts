324315df8cbc07388af03a884fc5036f306301e7
Josiah Voight <jovoight@gmail.com>
2024-03-15 20:01:31 -0500

    fixed bug in build production

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,8 +1,5 @@
 import {
-    useQuery,
     useMutation,
-    useQueryClient,
-    useInfiniteQuery
 } from '@tanstack/react-query'
 import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
 import { INewUser } from '@/types';
